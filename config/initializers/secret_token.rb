c82ea09aba69609a9dda91138fc1ee371bdeb668
Dean Poulin <dean.poulin@gmail.com>
2011-06-02 20:26:21 -0400

    added all initial files for convergence.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convergence::Application.config.secret_token = '7dcd1ffcdc346d6857c5cfc9870316afe05c7fdaf6f35d4c647d8feb571a30741bbf9718a0a804377b920413e8339e293d596dff07ccecd23901c22de1898314'
